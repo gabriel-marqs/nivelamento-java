be726b54f975a64f295d52be1e83a6a5067d2295
Gabriel Marques <79283546+gabriel-marqs@users.noreply.github.com>
2022-12-28 15:23:50 -0300

    Update uri1116.java
    Como no Java ao tentarmos dividir 0 por algo da consta o resultado "infinity", adicionei mais um "if else" para realizar um print de "0.0" e assim ficar de acordo com o exercício proposto.

--- src/uri1116.java
+++ src/uri1116.java
@@ -1,27 +1,32 @@
 import java.util.Scanner;
-
-public class uri1116 {
+import java.util.Locale;
+public class ex004 {
 
 	public static void main(String[] args) {
-
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
+		
 		int n = sc.nextInt();
 		
-		for (int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
+			double n1 = sc.nextDouble();	
+			double n2 = sc.nextDouble();
 			
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			
-			if (y == 0) {
-				System.out.println("divisao impossivel");
+			if (n2 == 0) {
+				System.out.println("Divisão Impossível.");
+			}
+			else if (n1 == 0) {
+				System.out.println("0.0");	
 			}
 			else {
-				double div = (double) x / y;
-				System.out.printf("%.1f%n", div);
+				double res = n1/n2;
+				System.out.printf("%.1f%n", res);
+				
 			}
 		}
 		
 		sc.close();
+
 	}
+
 }
